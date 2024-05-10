fadfeb1df8b8d4b6f99c8de668dd18e08f73d701
Ogheneyoma <Lawrenceyoma@gmail.com>
2024-05-10 10:18:07 +0100

    feat: added authentication with clerk

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,14 +2,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
-      <Link 
-        href="/"
-        className="flex items-center gap-1"
-      >
+      <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
           width={32}
@@ -17,16 +15,20 @@ const Navbar = () => {
           alt="Yoom logo"
           className="max-sm:size-10"
         />
-        <p className="text-[26px] font-extrabold text-white max-sm:hidden">Yooma</p>
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Yooma
+        </p>
       </Link>
 
       <div className="flex-between gap-5">
         {/* clerk */}
-
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <MobileNav />
       </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar
